ebce3f7464f9363778aeb2f9ce00de5da5fbb995
quekhoait <166801979+quekhoait@users.noreply.github.com>
2025-06-13 20:20:03 +0700

    kk

--- frontend/my-dev-app/src/index.js
+++ frontend/my-dev-app/src/index.js
@@ -8,12 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-
-    ưdgshjdhs
   </React.StrictMode>
 );
-
-///asdfdsfasdkfnasdfiladjsnfsdikdnfbladsjk
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
