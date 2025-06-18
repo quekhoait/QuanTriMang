af1d58dc96b8a485111de2662648b750c9b44fa5
quekhoait <166801979+quekhoait@users.noreply.github.com>
2025-06-18 23:08:51 +0700

    Cn

--- backend/src/controller/UserController.js
+++ backend/src/controller/UserController.js
@@ -5,23 +5,25 @@ const JwtServices = require("../services/jwtServices")
 
 const createUser = async(req, res)=>{
   try{
-    const {username, password, confirmPassword} = req.body;
-    if(!username || !password || !confirmPassword){
+    const {email, username, password} = req.body;
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      const isCheckEmail = emailRegex.test(email)
+    if(!email || !username || !password ){
       return res.status(400).json({
         status: "ERR",
         message: "Bạn cần nhập đầy đủ thông tin"
-      })
-    }
-    if(confirmPassword!== password){
-      return res.status(400).json({
-        status: "ERR",
-        message: "Mật khẩu xác nhận chưa đúng"
       })
     }
      if(password.length < 6){
       return res.status(400).json({
         status: "ERR",
         message: "Mật khẩu phải dài hơn 6 ký tự"
+      })
+    }
+    if(!isCheckEmail){
+      return res.status(400).json({
+          status: 'ERR',
+          message:'Email không hợp lệ'
       })
     }
     const user = await UserServices.createUser(req.body);
