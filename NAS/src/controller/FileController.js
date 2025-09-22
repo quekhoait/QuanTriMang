6d6633bace5b8df2bd82834353aab9bb498316a0
quekhoait <166801979+quekhoait@users.noreply.github.com>
2025-09-22 18:47:58 +0700

    up

--- NAS/src/controller/FileController.js
+++ NAS/src/controller/FileController.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const ParentFolder = 'C:\\uploads\\NAS\\Demo';
-const libre = require("libreoffice-convert");
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
